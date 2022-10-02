cb38e0f1673ac637bce630490962b3fccd52783b
JooJo <sjak0101@gmail.com>
2022-10-02 09:41:06 +0100

    react node mongo crud

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const AddUser = () => {
+
+    const handleAddUser = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user ={name, email};
+
+        // send data to the server
+
+        fetch('http://localhost:5000/user',{
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log('success', data);
+            alert('users addded successful !!!');
+            event.target.reset();
+        })
+    }
+
+    return (
+        <div>
+            <h2>Please add a new user</h2>
+            <form onSubmit={handleAddUser}>
+                <input type='text' name='name' placeholder='name' required/>
+                <br />
+                <input type='email' name='email' placeholder='Email' required/>
+                <br />
+                <input type="submit" value="Add User" />
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
